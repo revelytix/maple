d3c4f63c34b16ac520a659d502ddc0a759354767
Ryan Senior <senior.ryan@gmail.com>
2012-05-07 08:29:41 -0500

    Removed the clj source path (fixes install), added a Revelytix-release suffix

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
-(defproject com.twitter/maple "0.1.7"
-  :source-path "src/clj"
+(defproject com.twitter/maple "0.1.7-rev"
+  ;;:source-path "src/clj"
   :java-source-path "src/jvm"
   :description "All the Cascading taps we have to offer."
   :repositories {"conjars" "http://conjars.org/repo/"}
